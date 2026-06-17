fe4c42c05e3bfdc68db95587e0d4ec68d9490a5e
Douglas <douglasd3v.contato@gmail.com>
2026-06-17 15:55:27 -0300

    Feat: Creating posts model

---
+++ src/models/Post.js
@@ -0,0 +1,28 @@
+import { DataTypes } from "sequelize";
+
+const Post = sequelize.define("Post", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  content: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  authorId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  published: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+    allowNull: false,
+  },
+});
+
+export default Post;
