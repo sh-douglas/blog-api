2734b4b261836327632da6187f5f50f7666ef54f
Douglas <douglasd3v.contato@gmail.com>
2026-06-18 09:38:48 -0300

    chore: add error middleware on app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 import express from "express";
 import cors from "cors";
 
+import globalErrorHandler from "./middlewares/GlobalError.js";
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use(globalErrorHandler);
 
 export default app;
