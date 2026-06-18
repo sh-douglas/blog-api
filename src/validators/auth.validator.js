56c1866763ed1a1685132e80d5b9cb20a8ef9b21
Douglas <douglasd3v.contato@gmail.com>
2026-06-18 09:37:50 -0300

    feat: add auth validation schemas

---
+++ src/validators/auth.validator.js
@@ -0,0 +1,22 @@
+import { z } from "zod";
+
+const registerSchema = z
+  .object({
+    name: z.string().trim().min(3, "The name must be 3 characters or more."),
+    email: z.string().trim().lowercase().email(),
+    password: z.string().min(6, "The password must be 6 characters or more"),
+    confirmPassword: z
+      .string()
+      .min(6, "The password must be 6 characters or more"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "The passwords don't match.",
+    path: ["confirmPassword"],
+  });
+
+const loginSchema = z.object({
+  email: z.string().trim().lowercase().email(),
+  password: z.string().min(6, "The password must be 6 characters or more"),
+});
+
+export { registerSchema, loginSchema };
