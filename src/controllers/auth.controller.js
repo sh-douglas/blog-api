c6613cd185aff487e40d8f693407a0dd401197a3
Douglas <douglasd3v.contato@gmail.com>
2026-06-18 10:44:19 -0300

    feat: add auth controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -10,6 +10,16 @@ class AuthController {
       next(error);
     }
   }
+
+  async login(req, res, next) {
+    try {
+      const user = await AuthService.login(req.body);
+
+      return res.status(200).json(user);
+    } catch (error) {
+      next(error);
+    }
+  }
 }
 
 export default new AuthController();
