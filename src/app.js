56c121d0ec7a6486c74f44e7c3a0a18837db9a3b
Douglas <douglasd3v.contato@gmail.com>
2026-06-18 10:43:30 -0300

    feat: add auth routes

--- src/app.js
+++ src/app.js
@@ -2,11 +2,16 @@ import express from "express";
 import cors from "cors";
 
 import globalErrorHandler from "./middlewares/GlobalError.js";
+import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
 
 app.use(globalErrorHandler);
 
